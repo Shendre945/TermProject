ffacb78ed2d068dfcda78913261c2ac6a3ff7d93
Shendre945 <152568186+Shendre945@users.noreply.github.com>
2023-12-05 22:47:24 -0500

    Updated Music Screen

--- app/src/main/java/com/example/bv/MainActivity.java
+++ app/src/main/java/com/example/bv/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         btmusic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SitesofIndia.class));
+                startActivity(new Intent(MainActivity.this, FolkMusic.class));
             }
         });
 
